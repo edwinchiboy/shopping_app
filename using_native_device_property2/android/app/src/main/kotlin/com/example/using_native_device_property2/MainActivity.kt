517b0d5e87fae22859e7ce4614aa6ef377933bcf
edwinchiboy <choboy.edeh17@gmail.com>
2022-02-26 16:41:55 +0100

    Udgrade 2

---
+++ using_native_device_property2/android/app/src/main/kotlin/com/example/using_native_device_property2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.using_native_device_property2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ using_native_device_property2/lib/screens/add_place_screen.dart
@@ -0,0 +1,78 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../widgets/image_input.dart';
+import '../providers/great_places.dart';
+import '../widgets/location_input.dart';
+
+class AddPlaceScreen extends StatefulWidget {
+  const AddPlaceScreen({Key? key}) : super(key: key);
+  static const routeName = '/add-place';
+
+  @override
+  _AddPlaceScreenState createState() => _AddPlaceScreenState();
+}
+
+class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  final _titleController = TextEditingController();
+  File? _pickedImage;
+
+  void _selectImage(File pickedImage) {
+    _pickedImage = pickedImage;
+  }
+
+  void _savedPlace() {
+    if (_titleController.text.isEmpty || _pickedImage == null) {
+      return;
+    }
+    Provider.of<GreatPlaces>(context, listen: false)
+        .addplace(_titleController.text, _pickedImage!);
+    Navigator.of(context).pop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add a New Place'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: const InputDecoration(labelText: 'Title'),
+                      controller: _titleController,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    ImageInput(_selectImage),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const LocationInput(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          RaisedButton.icon(
+            onPressed: _savedPlace,
+            icon: const Icon(Icons.add),
+            label: const Text('Add place'),
+            elevation: 0,
+            color: Theme.of(context).accentColor,
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          ),
+        ],
+      ),
+    );
+  }
+}
